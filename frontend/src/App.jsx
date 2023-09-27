4de7920b2c3e66ec0d2b3467797cd1114ae6a14a
David García Garzón <david.garcia@somenergia.coop>
2023-09-27 11:04:52 +0200

    🚧 somoffice shell integration (not working)
    Co-authored-by: Marite Guerrieri <marite.guerrieri@somenergia.coop>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
+import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { SomofficeShell } from 'somoffice-shell'
+import { BrowserRouter as Router } from "react-router-dom";
+
+const login = () => undefined
+const logout = () => undefined
+const getUserInfo = () => {
+  return {
+    role: 'admin', // rol de la persona
+    fullname: 'Bender Bending Rodríguez', // nombre completo de la persona
+    language: 'es', // idioma en el que la persona verá su OV
+    token: 'random-token', // token para las peticiones API
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const rawTheme = {
+    typography: {
+      fontFamily: 'Montserrat, Heletica, sansserif',
+    },
+  }
+  const settings = {}
+  const config = {
+    base_url: 'http://localhost:5500', // required: url de la api donde deben hacerse las peticiones
+    theme: rawTheme, // optional
+    settings: settings, // optional
+    logo: undefined, // TODO: check this
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <SomofficeShell
+          config={config}
+          login={login}
+          logout={logout}
+          getUserInfo={getUserInfo}
+          keycloak={{}}
+          components={{}}
+        ></SomofficeShell>
+      </Router>
     </>
   )
 }
-
+/*
+    keycloak={{ token: 'aaaaa' }} // TODO: remove this
+    sections={sections} // optional
+*/
 export default App
