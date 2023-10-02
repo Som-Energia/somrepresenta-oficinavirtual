b9d0a1d81f2f5daed9f58da342d4868855ff8a30
David García Garzón <david.garcia@somenergia.coop>
2023-10-02 13:34:33 +0200

    ✨ Minimal integrated backend
    Co-authored-by: Marite Guerrieri <marite.guerrieri@somenergia.coop>

---
+++ scripts/representa_api.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import click
+from consolemsg import warn, step
+from backend import __version__
+import os
+
+@click.command()
+@click.help_option()
+@click.version_option(__version__)
+
+@click.option('--debug',
+    is_flag=True,
+    help="Runs in debug mode",
+    )
+@click.option('--host', '-h',
+    default='0.0.0.0',
+    help="The address to listen to",
+    )
+@click.option('--port', '-p',
+    type=int,
+    default=5500,
+    help="The port to listen to",
+    )
+@click.option('--printrules',
+    is_flag=True,
+    help="Prints the url patterns being serverd",
+    )
+def main(debug, host, port, printrules):
+    "Runs the Tomatic web and API"
+    print(debug, host, port, printrules)
+
+    step("Starting API")
+    if printrules:
+        for rule in app.routes:
+            step("- {}", rule.path)
+    import uvicorn
+    uvicorn.run("backend.api:app", host=host, port=port, reload=debug)
+    step("API stopped")
+
+if __name__=='__main__':
+    main()
+
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: '../backend/dist',
+  },
   root: 'frontend',
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5500',
+    },
+  },
 })
