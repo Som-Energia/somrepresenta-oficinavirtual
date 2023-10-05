487f8327ea112577b334ecbc4b56d765a3a53848
David García Garzón <david.garcia@somenergia.coop>
2023-10-05 10:13:39 +0200

    ✨ NotYetImplementedPage
    Co-authored-by: Marite Guerrieri <marite.guerrieri@somenergia.coop>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import './i18n/i18n'
@@ -7,6 +7,7 @@ import GlobalTheme from './components/GlobalTheme'
 import TestPage from './components/TestPage'
 import HomePage from './HomePage'
 import NotFoundPage from './NotFoundPage'
+import NotYetImplementedPage from './NotYetImplementedPage'
 import DialogProvider from './components/DialogProvider'
 import AuthProvider from './components/AuthProvider'
 
@@ -19,13 +20,29 @@ const routes = [
     path: '/',
     element: <HomePage />,
   },
+  {
+    path: '/install',
+    element: <NotYetImplementedPage />
+  },
+  {
+    path: '/invoices',
+    element: <NotYetImplementedPage />
+  },
+  {
+    path: '/production',
+    element: <NotYetImplementedPage />
+  },
+  {
+    path: '/test',
+    element: <TestPage />
+  },
 ]
 
 const router = createBrowserRouter(routes)
 
 function App() {
   const { t, i18n } = useTranslation()
-  const [count, setCount] = useState(0)
+  const [count, setCount] = React.useState(0)
 
   return (
     <GlobalTheme>
