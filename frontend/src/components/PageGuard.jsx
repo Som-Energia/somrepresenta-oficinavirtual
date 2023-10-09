7266a6f9d6e71c4942f7c7ba76ecb183d5c6cf15
David García Garzón <david.garcia@somenergia.coop>
2023-10-09 11:51:34 +0200

    ✨ Nicer Not Authenticated Page
    Co-authored-by: Marite Guerrieri <marite.guerrieri@somenergia.coop>

--- frontend/src/components/PageGuard.jsx
+++ frontend/src/components/PageGuard.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import BlockIcon from '@mui/icons-material/Block'
+import { useTranslation } from 'react-i18next'
 import { useAuth } from './AuthProvider'
 import AppFrame from './AppFrame'
 
 function NotAuthenticated() {
+  const { t, i18n } = useTranslation()
+  const { login } = useAuth()
   return (
     <AppFrame>
-      <h1>Not Authenticated</h1>
+      <Container
+        sx={{
+          display: 'flex',
+          flexFlow: 'column',
+          alignItems: 'center',
+          p: 2,
+        }}
+      >
+        <Typography variant="h4">{t('APP_FRAME.USER_REQUIRED')}</Typography>
+
+        <BlockIcon
+          sx={{
+            fontSize: '10rem',
+            marginBlock: '2rem',
+          }}
+        />
+        <Box sx={{ display: 'flex', gap: 3 }}>
+          <Button variant="contained" color="primary" onClick={() => login()}>
+            {t('APP_FRAME.LOGIN')}
+          </Button>
+        </Box>
+      </Container>
     </AppFrame>
   )
 }
