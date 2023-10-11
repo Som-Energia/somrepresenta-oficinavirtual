7505e3f616487a6242315eda48d91b5524d3a81e
joana <joana.figueira@somenergia.coop>
2023-10-11 17:14:56 +0200

    🎨  Update Footer format
     - Add translation strings
     - Delete footeritems.yaml
     - Align text

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,12 +6,26 @@ import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import { useNavigate } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
-import footerItems from '../data/footeritems.yaml'
 
 export default function Footer(params) {
   const { t, i18n } = useTranslation()
   const navigate = useNavigate()
   const { sx } = params
+
+  const footerItems = [
+    {
+      text: t('APP_FRAME.DISCLAIMER'),
+      url: t('APP_FRAME.DISCLAIMER_URL'),
+    },
+    {
+      text: t('APP_FRAME.PRIVACY_POLICY'),
+      url: t('APP_FRAME.PRIVACY_POLICY_URL'),
+    },
+    {
+      text: 'SOM ENERGIA SCCL',
+      url: 'https://somenergia.coop',
+    },
+  ]
   return (
     <Paper
       color="primary"
@@ -25,7 +39,7 @@ export default function Footer(params) {
       }}
     >
       <Container>
-        <Box sx={{ display: 'flex', flexFlow: 'row wrap', gap: 1 }}>
+       <Box sx={{ display: 'flex', flexFlow: 'row wrap', gap: 1, justifyContent: 'space-around' }}>
           {footerItems.map((item) => (
             <Button
               key={item.text}
