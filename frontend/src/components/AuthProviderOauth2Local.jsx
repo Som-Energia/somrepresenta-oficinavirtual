0113ca59fb1448ddf153cdde594fe500ccd24828
David García Garzón <david.garcia@somenergia.coop>
2023-10-25 20:21:54 +0200

    ♻️ extracted ChangePasswordDialog

--- frontend/src/components/AuthProviderOauth2Local.jsx
+++ frontend/src/components/AuthProviderOauth2Local.jsx
@@ -15,6 +15,7 @@ import { useDialog } from './DialogProvider'
 import authProviders from '../data/authproviders.yaml'
 import ov from '../services/ovapi'
 import wait from '../services/wait'
+import ChangePasswordDialog from './ChangePasswordDialog'
 
 const noFunction = () => undefined
 
@@ -77,128 +78,6 @@ function AuthProviderDialog(params) {
                   ? t('LOGIN.SUBMIT_BUTTON_PROCESSING')
                   : t('LOGIN.SUBMIT_BUTTON_LOGIN')}
               </Button>
-            </DialogActions>
-          </Stack>
-        </form>
-      </DialogContent>
-    </>
-  )
-}
-
-function ChangePasswordDialog(params) {
-  const { closeDialog } = params
-  const [currentPassword, setCurrentPassword] = React.useState('')
-  const [newPassword, setNewPassword] = React.useState('')
-  const [checkPassword, setCheckPassword] = React.useState('')
-  const [error, setError] = React.useState()
-  const [isLoading, beLoading] = React.useState(false)
-  const [isSuccess, beSuccess] = React.useState(false)
-  const { t, i18n } = useTranslation()
-
-  async function handleSubmit(ev) {
-    ev.preventDefault()
-    setError(undefined)
-    beLoading(true)
-    beSuccess(false)
-    try {
-      const result = await ov.localChangePassword(currentPassword, newPassword)
-      beLoading(false)
-      beSuccess(true)
-      wait(1000).then(() => closeDialog())
-    } catch (error) {
-      setError(t('CHANGE_PASSWORD.CURRENT_PASSWORD_MISSMATCH_ERROR'))
-      beLoading(false)
-    }
-  }
-  const newPasswordError =
-    newPassword === ''
-      ? undefined
-      : newPassword.length < 8
-      ? t('CHANGE_PASSWORD.NEW_PASSWORD_TOO_SHORT_ERROR')
-      : newPassword === currentPassword
-      ? t('CHANGE_PASSWORD.NEW_PASSWORD_SAME_AS_CURRENT_ERROR')
-      : undefined
-
-  const checkPasswordError =
-    checkPassword === ''
-      ? undefined
-      : checkPassword !== newPassword
-      ? t('CHANGE_PASSWORD.NEW_PASSWORD_MISSMATCH_ERROR')
-      : undefined
-
-  return (
-    <>
-      <DialogTitle>{t('CHANGE_PASSWORD.CHANGE_PASSWORD_DIALOG_TITLE')}</DialogTitle>
-      <DialogContent sx={{ minWidth: '20rem' }}>
-        <form onSubmit={handleSubmit} method="post">
-          <Stack spacing={2} sx={{ pt: 1 }}>
-            <TextField
-              name="currentPassword"
-              label={t('CHANGE_PASSWORD.CURRENT_PASSWORD_LABEL')}
-              type="password"
-              value={currentPassword}
-              onChange={(ev) => {
-                setCurrentPassword(ev.target.value)
-                setError(undefined)
-              }}
-              error={!!error}
-              helperText={error || ' '}
-            />
-            <TextField
-              name="newPassword"
-              label={t('CHANGE_PASSWORD.NEW_PASSWORD_LABEL')}
-              type="password"
-              value={newPassword}
-              onChange={(ev) => {
-                setNewPassword(ev.target.value)
-              }}
-              error={!!newPasswordError}
-              helperText={
-                newPasswordError || ' ' // To avoid relayout when no error
-              }
-            />
-            <TextField
-              name="checkPassword"
-              label={t('CHANGE_PASSWORD.CHECK_PASSWORD_LABEL')}
-              type="password"
-              value={checkPassword}
-              onChange={(ev) => setCheckPassword(ev.target.value)}
-              error={checkPassword !== '' && checkPassword !== newPassword}
-              helperText={
-                checkPasswordError || ' ' // To avoid relayout when no error
-              }
-            />
-            <DialogActions sx={{ display: 'flex', justifyContent: 'right', gap: 2 }}>
-              {isSuccess ? (
-                <Alert severity="success">
-                  {t('CHANGE_PASSWORD.SUBMIT_BUTTON_CHANGED_PASSWORD')}
-                </Alert>
-              ) : (
-                <>
-                  <Button onClick={closeDialog}>Cancel</Button>
-                  <Button
-                    type="submit"
-                    variant="contained"
-                    disabled={
-                      // sending or done
-                      isLoading ||
-                      isSuccess ||
-                      // Any field empty
-                      !currentPassword ||
-                      !newPassword ||
-                      !checkPassword ||
-                      // Any field in error
-                      !!error ||
-                      !!newPasswordError ||
-                      !!checkPasswordError
-                    }
-                  >
-                    {isLoading
-                      ? t('CHANGE_PASSWORD.SUBMIT_BUTTON_CHANGING_PASSWORD')
-                      : t('CHANGE_PASSWORD.SUBMIT_BUTTON_CHANGE_PASSWORD')}
-                  </Button>
-                </>
-              )}
             </DialogActions>
           </Stack>
         </form>
