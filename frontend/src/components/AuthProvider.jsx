724b7fe203e1559878d9cd02a7714a41a70df289
David García Garzón <david.garcia@somenergia.coop>
2023-10-27 19:17:46 +0200

    🧹 removed console logs from AuthProvider

--- frontend/src/components/AuthProvider.jsx
+++ frontend/src/components/AuthProvider.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
-console.log(import.meta.env)
+
 const configuredBackend = import.meta.env.VITE_AUTH_BACKEND ?? 'Oauth2'
 
+// TODO: This would be a better approach but does not work in build just in dev
+//const { AuthProvider, useAuth } = import(`./AuthProvider${configuredBackend}.jsx`)
+
 const backends = import.meta.globEager('./AuthProvider?*.jsx')
-console.log({ backends })
 const selectableBackends = Object.fromEntries(
   Object.keys(backends).map((key) => {
     const code = key.slice('./AuthProvider'.length, -'.jsx'.length)
     return [code, backends[key]]
   }),
 )
-console.log({ selectableBackends })
-console.log({ configuredBackend })
-
-//const { AuthProvider, useAuth } = import(`./AuthProvider${configuredBackend}.jsx`)
 const { AuthProvider, useAuth } = selectableBackends[configuredBackend]
-console.log({ AuthProvider, useAuth })
 
 export { useAuth }
 export default AuthProvider
