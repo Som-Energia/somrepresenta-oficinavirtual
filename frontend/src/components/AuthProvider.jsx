140718357bd2523c704509bbf95aaf6f97593b87
David García Garzón <david.garcia@somenergia.coop>
2023-10-27 18:43:45 +0200

    🐛 AuthProvider: different dynamic import approach
    Production build gave errors when importing an string
    interpolated path.
    Workaround: load them all like we do with languages
    and choose the proper one.

--- frontend/src/components/AuthProvider.jsx
+++ frontend/src/components/AuthProvider.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 console.log(import.meta.env)
 const configuredBackend = import.meta.env.VITE_AUTH_BACKEND ?? 'Oauth2'
 
+const backends = import.meta.globEager('./AuthProvider?*.jsx')
+console.log({ backends })
+const selectableBackends = Object.fromEntries(
+  Object.keys(backends).map((key) => {
+    const code = key.slice('./AuthProvider'.length, -'.jsx'.length)
+    return [code, backends[key]]
+  }),
+)
+console.log({ selectableBackends })
 console.log({ configuredBackend })
 
-const { AuthProvider, useAuth } = await import(`./AuthProvider${configuredBackend}.jsx`)
+//const { AuthProvider, useAuth } = import(`./AuthProvider${configuredBackend}.jsx`)
+const { AuthProvider, useAuth } = selectableBackends[configuredBackend]
 console.log({ AuthProvider, useAuth })
 
 export { useAuth }
