f6027ff24a41f759a83f5842cfd816b11eb20a25
David García Garzón <david.garcia@somenergia.coop>
2023-11-17 14:47:46 +0100

    🧹 firstPendingDocument: cleanup not TDD code

--- frontend/src/services/signatures.js
+++ frontend/src/services/signatures.js
@@ -6,23 +6,12 @@ function firstPendingDocument(requiredDocuments, signedDocuments) {
   const signed = Object.fromEntries(signedDocuments.map((d) => [d.document, d.version]))
   for (const required of requiredDocuments) {
     const signedVersion = signed[required.document]
-    if (signedVersion === undefined) return required
-    if (signedVersion < required.version) return required
-  }
-  return null
-
-  /*
-  signedDocuments = signedDocuments || []
-  const signed = Object.fromEntries(signedDocuments.map((d) => [d.document, d.version]))
-  for (const required of requiredDocuments) {
-    const signedVersion = signed[required.document]
     // Not signed ever
     if (signedVersion === undefined) return required
     // Signed but an older version
     if (signedVersion < required.version) return required
   }
   return null
-  */
 }
 
 export { firstPendingDocument }
