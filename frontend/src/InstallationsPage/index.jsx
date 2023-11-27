eb817048761f5ae21bc861110087803e402fee45
David García Garzón <david.garcia@somenergia.coop>
2023-11-27 21:40:56 +0100

    ✨ search icon
    Co-authored-by: Joana Figueira <joana.figueira@somenergia.coop>

--- frontend/src/InstallationsPage/index.jsx
+++ frontend/src/InstallationsPage/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import SearchIcon from '@mui/icons-material/Search'
 import { useTranslation } from 'react-i18next'
 import TableEditor from '../components/TableEditor'
 import dummyData from '../data/dummyinstallations.yaml'
@@ -6,25 +7,31 @@ import dummyData from '../data/dummyinstallations.yaml'
 export default function InstalltionsPage(params) {
   const { t, i18n } = useTranslation()
   const rows = dummyData
-  const columns=[
+  const columns = [
     {
-        id: "id", // TODO: can we name it contract?
-        label: t("INSTALLATIONS.COLUMN_CONTRACT_NUMBER"),
-        searchable: true,
-        numeric: false,
-        disablePadding: true,
+      id: 'id', // TODO: can we name it contract?
+      label: t('INSTALLATIONS.COLUMN_CONTRACT_NUMBER'),
+      searchable: true,
+      numeric: false,
+      disablePadding: true,
     },
     {
-        id: "name",
-        label: t("INSTALLATIONS.COLUMN_INSTALLATION_NAME"),
-        searchable: true,
-        numeric: false,
-        disablePadding: false,
+      id: 'name',
+      label: t('INSTALLATIONS.COLUMN_INSTALLATION_NAME'),
+      searchable: true,
+      numeric: false,
+      disablePadding: false,
     },
   ]
-  const actions=[]
-  const selectionActions=[]
-  const itemActions=[]
+  const actions = []
+  const selectionActions = []
+  const itemActions = [
+    {
+      title: 'Details',
+      icon: <SearchIcon fontSize="inherit" />,
+      handler: (item)=>{console.log("Voy!!", item)},
+    },
+  ]
   return (
     <TableEditor
       title={t('INSTALLATIONS.TABLE_TITLE')}
