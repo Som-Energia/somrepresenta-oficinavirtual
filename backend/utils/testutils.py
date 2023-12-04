4a7bbd2ffaefa43d0636e736d905aaa0f59fbfcb
David García Garzón <david.garcia@somenergia.coop>
2023-12-04 18:00:43 +0100

    🐛 environ contexthandler: edge case
    - actual value returned as context
    - restoring crashed when inside value is None
    - value was set twice
    - Only None unsets, '' does not
    - Added doctest for all those cases

--- backend/utils/testutils.py
+++ backend/utils/testutils.py
@@ -3,16 +3,51 @@ from contextlib import contextmanager
 
 @contextmanager
 def environ(var, value):
+    """
+    Context handler to modify environment variables
+    while inside the context and restore them on exit.
+    Use None as value to unset.
+
+    >>> import os
+    >>> os.environ['DEFINED_VAR'] = "Previous value"
+    
+    Restores old values of defined variables
+    >>> with environ('DEFINED_VAR', 'New value'):
+    ...    os.environ.get('DEFINED_VAR')
+    'New value'
+    >>> os.environ.get('DEFINED_VAR')
+    'Previous value'
+
+    Clears variable if set to None
+    >>> with environ('DEFINED_VAR', None):
+    ...    'DEFINED_VAR' in os.environ
+    False
+    >>> 'DEFINED_VAR' in os.environ
+    True
+
+    Restores indefinition on undefined variables
+    >>> with environ('UNDEFINED_VAR', 'value'):
+    ...    os.environ.get('UNDEFINED_VAR')
+    'value'
+    >>> os.environ.get('UNDEFINED_VAR') # Nothing
+
+    Behave on exception
+    >>> try:
+    ...     with environ('DEFINED_VAR', 'New'):
+    ...         raise Exception("Error")
+    ... except: pass
+    >>> os.environ.get('DEFINED_VAR')
+    'Previous value'
+    """
     def set(value):
-        if value:
+        if value is not None:
             os.environ[var]=value
-        else:
+        elif var in os.environ:
             del os.environ[var]
     
     oldvalue = os.environ.get(var)
     set(value)
-    os.environ[var]=value
     try:
-        yield
+        yield value
     finally:
         set(oldvalue)
