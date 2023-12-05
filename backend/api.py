edd55855018bc2580219598d54b9dd3eba95b337
David García Garzón <david.garcia@somenergia.coop>
2023-12-05 14:10:37 +0100

    ♻️ extract api_business module
    - Contains all the business related entrypoints
    - To be splitted if it gets too populated
    - Added error handler for ErpConnectionError

--- backend/api.py
+++ backend/api.py
@@ -1,12 +1,10 @@
-from fastapi import FastAPI, Request, Depends
+from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse
 from pathlib import Path
 from dotenv import load_dotenv
 from . import __version__ as version
-from .auth import setup_auth, validated_user
 from .authlocal import setup_authlocal
-from .models import UserProfile, TokenUser, SignatureResult, InstallationSummary
-from .datasources import profile_info, sign_document, installation_list
+from .api_business import setup_business
 
 load_dotenv()
 app = FastAPI()
@@ -29,18 +27,7 @@ def apiVersion():
         version = version,
     )
 
-@app.get('/api/me')
-def apiMe(user: dict = Depends(validated_user)) -> UserProfile:
-    return profile_info(user)
-
-@app.post('/api/sign_document/{document}')
-def apiSignDocument(document: str, user: dict = Depends(validated_user)) -> SignatureResult:
-    return sign_document(user['username'], document)
-
-@app.get('/api/installations')
-def apiInstallations(user: dict = Depends(validated_user)) -> list[InstallationSummary]:
-    return installation_list(user['username'])
 
 #setup_auth(app)
 setup_authlocal(app)
-
+setup_business(app)
