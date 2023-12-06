f693a1fda1f8278adfa47d9d95630b4ebea1a833
David García Garzón <david.garcia@somenergia.coop>
2023-12-06 12:45:01 +0100

    ✅ testing common fastapi http errors

--- backend/api_test.py
+++ backend/api_test.py
@@ -1,4 +1,5 @@
 import unittest
+from fastapi import status
 from fastapi.testclient import TestClient
 from yamlns import ns
 from . import __version__ as api_version
@@ -14,6 +15,26 @@ class VersionApi_Test(unittest.TestCase):
 
     def test_version(self):
         r = self.client.get('/api/version')
+        self.assertResponseEqual(r, f"""
+            version: {api_version}
+        """)
+
+    def test_missing_file(self):
+        r = self.client.get('/missing_file')
+        self.assertResponseEqual(r, f"""
+            detail: Not Found
+        """, status.HTTP_404_NOT_FOUND)
+
+    def test_bad_method(self):
+        r = self.client.post('/')
+        self.assertResponseEqual(r, f"""
+            detail: Method Not Allowed
+        """, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_validation_error(self):
+        r = self.client.get('/api/version', params=dict(
+            unexpected='value',
+        ))
         self.assertResponseEqual(r, f"""
             version: {api_version}
         """)
