ed40cb882bc266a6e1e40d5016dae30093d84798
David García Garzón <david.garcia@somenergia.coop>
2023-12-06 16:06:08 +0100

    ✅ test static are deployed and retrieved

--- backend/api_test.py
+++ backend/api_test.py
@@ -2,6 +2,7 @@ import unittest
 from fastapi import status
 from fastapi.testclient import TestClient
 from yamlns import ns
+from pathlib import Path
 from . import __version__ as api_version
 from . import api
 
@@ -18,6 +19,16 @@ class VersionApi_Test(unittest.TestCase):
         self.assertResponseEqual(r, f"""
             version: {api_version}
         """)
+
+    def test_static(self):
+        # This test will fail if ui not deployed: make ui-deploy
+        r = self.client.get('/logo.svg')
+        r.raise_for_status()
+        self.assertResponseEqual(
+            r,
+            Path('frontend/public/logo.svg').read_text(),
+            content_type='image/svg+xml',
+        )
 
     def test_missing_file(self):
         r = self.client.get('/missing_file')
