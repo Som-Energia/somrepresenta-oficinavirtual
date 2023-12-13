c3276407c9ec9f5e5fec35dbe36467dc4c5d6296
David García Garzón <david.garcia@somenergia.coop>
2023-12-13 10:42:18 +0100

    💄 Error reporting on details

--- frontend/src/pages/DetailInstallationPage/index.jsx
+++ frontend/src/pages/DetailInstallationPage/index.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Container, Alert } from '@mui/material'
+import Box from '@mui/material/Box'
 import { useTranslation } from 'react-i18next'
-import ov from '../../services/ovapi'
+import ovapi from '../../services/ovapi'
 import Loading from '../../components/Loading'
 import PageTitle from '../../components/PageTitle'
 import SolarPowerIcon from '@mui/icons-material/SolarPower'
 import SimpleTable from '../../components/SimpleTable'
+import ErrorSplash from '../../components/ErrorSplash'
 
 // TODO: create or use a generic context to set app alert messages
 const AlertError = ({ error }) => {
@@ -21,8 +23,8 @@ const AlertError = ({ error }) => {
 export default function DetailInstallationPage(params) {
   const { contract_number } = useParams()
   const { t } = useTranslation()
-  const [installationDetail, setInstallationDetail] = useState(false)
-  const [contractDetail, setContractDetail] = useState(false)
+  const [installationDetail, setInstallationDetail] = useState(undefined)
+  const [contractDetail, setContractDetail] = useState(undefined)
   const [error, setError] = useState(false)
 
   useEffect(() => {
@@ -40,36 +42,46 @@ export default function DetailInstallationPage(params) {
   }
 
   async function getDetailInstallation() {
-    try {
-      const result = await ov.installationDetails(contract_number)
-      setInstallationDetail(result?.installation_details)
-      const contractData = transformContractDetails(result?.contract_details)
-      setContractDetail(contractData)
-    } catch (e) {
-      // TODO: check how errors are managed
-      setError(e.code)
+    setError(false)
+    setInstallationDetail(undefined)
+    setContractDetail(undefined)
+    const result = await ovapi.installationDetails(contract_number)
+    if (!result) {
+      setError(true)
+      return
     }
+    setInstallationDetail(result?.installation_details)
+    const contractData = transformContractDetails(result?.contract_details)
+    setContractDetail(contractData)
   }
 
-  return error ? (
-    <AlertError error={error} />
-  ) : installationDetail && contractDetail ? (
+  return !error && (!installationDetail || !contractDetail) ? (
+    <Loading />
+  ) : (
     <Container>
       <PageTitle Icon={SolarPowerIcon}>
         {t('INSTALLATION_DETAIL.DETAILS_TITLE')}
       </PageTitle>
-      <SimpleTable
-        fields={installationDetail}
-        translationsPrefix="INSTALLATION_DETAIL"
-        title={t('INSTALLATION_DETAIL.INSTALLATION_DETAILS_TITLE')}
-      />
-      <SimpleTable
-        fields={contractDetail}
-        translationsPrefix="CONTRACT_DETAIL"
-        title={t('CONTRACT_DETAIL.CONTRACT_DETAILS_TITLE')}
-      />
+      {error ? (
+        <ErrorSplash
+          message={t('INSTALLATION_DETAIL.ERROR_LOADING_DATA')}
+          backlink="/installation"
+          backtext={t('INSTALLATION_DETAIL.BACK_TO_INSTALLATIONS')}
+        />
+      ) : (
+        <>
+          <SimpleTable
+            fields={installationDetail}
+            translationsPrefix="INSTALLATION_DETAIL"
+            title={t('INSTALLATION_DETAIL.INSTALLATION_DETAILS_TITLE')}
+          />
+          <SimpleTable
+            fields={contractDetail}
+            translationsPrefix="CONTRACT_DETAIL"
+            title={t('CONTRACT_DETAIL.CONTRACT_DETAILS_TITLE')}
+          />
+        </>
+      )}
     </Container>
-  ) : (
-    <Loading />
   )
 }
