ff878c2a2879f70f33ab84ed273266062b6c468c
David García Garzón <david.garcia@somenergia.coop>
2023-12-19 11:46:29 +0100

    💄 currency formating addes thousands separator
    Co-authored-by: Marite Guerrieri <marite.guerrieri@somenergia.coop>

--- frontend/src/services/format.js
+++ frontend/src/services/format.js
@@ -3,11 +3,15 @@ import i18n from '../i18n/i18n'
 function euros(amount) {
   const asFloat = parseFloat(amount)
   if (isNaN(asFloat)) return '-- €'
-  const localized = asFloat.toLocaleString('es', {
+  const language = i18n.language
+  const localized = asFloat.toLocaleString(language, {
+    style: 'currency',
+    currency: 'EUR',
     maximumFractionDigits: 2,
     minimumFractionDigits: 2,
+    useGrouping: true,
   })
-  return `${localized} €`
+  return `${localized}`
 }
 
 function date(adate) {
--- frontend/src/services/format.test.js
+++ frontend/src/services/format.test.js
@@ -4,29 +4,50 @@ import i18n from '../i18n/i18n'
 
 
 describe('euros formatting', () => {
+  var previousLanguage
+  beforeEach(()=>{
+    previousLanguage = i18n.language
+    i18n.changeLanguage('es')
+  })
+  afterEach(()=>{
+    i18n.changeLanguage(previousLanguage)
+  })
   it('amount with no decimals', () => {
-    expect(euros(2)).toBe('2,00 €')
+    expect(euros(2)).toBe('2,00 €')
   })
   it('amount with two decimals', () => {
-    expect(euros(2.15)).toBe('2,15 €')
+    expect(euros(2.15)).toBe('2,15 €')
   })
   it('amount with rounding up', () => {
-    expect(euros(2.159)).toBe('2,16 €')
+    expect(euros(2.159)).toBe('2,16 €')
   })
   it('amount with rounding down', () => {
-    expect(euros(2.151)).toBe('2,15 €')
+    expect(euros(2.151)).toBe('2,15 €')
   })
   it('amount with rounding middle', () => {
-    expect(euros(2.155)).toBe('2,16 €')
+    expect(euros(2.155)).toBe('2,16 €')
   })
   it('amount with negative', () => {
-    expect(euros(-2)).toBe('-2,00 €')
+    expect(euros(-2)).toBe('-2,00 €')
   })
   it('amount with string number', () => {
-    expect(euros('-2')).toBe('-2,00 €')
+    expect(euros('-2')).toBe('-2,00 €')
   })
   it('amount with undefined', () => {
     expect(euros(undefined)).toBe('-- €')
+  })
+  it('amount over thousands puts point', () => {
+    i18n.changeLanguage('es')
+
+    expect(euros(2345)).toBe('2.345,00 €')
+  })
+  it('basque locale', () => {
+    i18n.changeLanguage('eu')
+    expect(euros(2345)).toBe('2.345,00 €')
+  })
+  it('english locale', () => {
+    i18n.changeLanguage('en')
+    expect(euros(2345)).toBe('€2,345.00')
   })
 })
 
