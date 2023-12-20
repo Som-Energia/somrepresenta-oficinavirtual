79fcd20d4058316ee668f71a7abc15bbe57294e2
javikalsan <1070397+javikalsan@users.noreply.github.com>
2023-12-20 17:09:50 +0100

    🧹 Context proposal cleanup

--- frontend/src/components/InstallationProvider.jsx
+++ frontend/src/components/InstallationProvider.jsx
@@ -5,22 +5,7 @@ import ovapi from '../services/ovapi'
 const InstallationContext = React.createContext()
 
 export default function InstallationProvider({ children }) {
-  // const [installations, setInstallations] = React.useState([])
   const { currentUser } = useAuth()
-
-  //const closeDialog = () => {
-  //  setDialogs((dialogs) => {
-  //    const latestDialog = dialogs.pop()
-  //    if (!latestDialog) return dialogs
-  //    if (latestDialog.onClose) latestDialog.onClose()
-  //    return [...dialogs].concat({ ...latestDialog, open: false })
-  //  })
-  //}
-
-
-  // const reloadUser = () => {
-  //   ov.currentUser().then((user) => setCurrentUser(user))
-  // }
 
   const getInstallations = () => {
     return ovapi.installations(currentUser)
@@ -36,13 +21,6 @@ export default function InstallationProvider({ children }) {
         // throw error;
       });
   };
-  
-
-  // const getInstallations = () => {
-  //   return [1,2,3]// Use the data here or perform additional actions
-  // };
-
-
 
   const contextValue = getInstallations()
 
