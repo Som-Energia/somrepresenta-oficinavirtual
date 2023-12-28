e525155fe25cc003331bca047e33930881e934ce
David García Garzón <david.garcia@somenergia.coop>
2023-12-28 02:59:28 +0100

    🏗️ depending on the published package by default

--- frontend/src/i18n/i18n.js
+++ frontend/src/i18n/i18n.js
@@ -1,7 +1,4 @@
-import i18n from 'i18next'
-import { initReactI18next } from 'react-i18next'
-import LanguageDetector from 'i18next-browser-languagedetector'
-import { addTranslationFiles } from '@somenergia/somenergia-ui/i18n'
+import i18n, { addTranslationFiles } from '@somenergia/somenergia-ui/i18n'
 
 const translationFiles = import.meta.glob('./locale-*.yaml', { eager: true })
 
