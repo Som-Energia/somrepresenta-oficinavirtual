97e27638956ba518ade0957476af9db9c0062dbe
David García Garzón <david.garcia@somenergia.coop>
2023-12-30 19:33:09 +0100

    🐛 vite-config: missing import

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteyaml from '@modyfi/vite-plugin-yaml'
 import svgr from 'vite-plugin-svgr'
+import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
