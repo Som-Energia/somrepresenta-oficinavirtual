4acabc90f5ca41d85eb3641a5868f6a167ca65ca
Beatrigz <beatriz.plana@somenergia.coop>
2024-01-26 14:08:00 +0100

    ✨ buttom to select bar or line type
     Co-authored-by: Beatrigz <beatriz.plana@somenergia.coop>
    Co-authored-by: Marta <marta.fernandez@somenergia.coop>

--- frontend/src/components/ChartProductionData.jsx
+++ frontend/src/components/ChartProductionData.jsx
@@ -1,17 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import Chart from '@somenergia/somenergia-ui/Chart'
-
 import ovapi from '../services/ovapi'
 
+import Grid from '@mui/material/Grid'
+import Switch from '@mui/material/Switch'
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
+import ToggleButton from '@mui/material/ToggleButton'
+
+import { useTranslation } from 'react-i18next'
+
+
+
 // TODO: Check what times do we need
-// const DAY = 'DAY'
-// const MONTH = 'MONTH'
-// const YEAR = 'YEAR'
+const DAILY = 'DAILY'
+const WEEKLY = 'WEEKLY'
+const MONTHLY = 'MONTHLY'
+const YEARLY = 'YEARLY'
+
+const LINE = 'LINE'
+const BAR = 'BAR'
+
 
 const ChartProductionData = () => {
   const [productionData, setProductionData] = useState([])
   const [compare, setCompare] = useState([])
-  const [mode, setMode] = useState(null)
+  const [type, setType] = useState(LINE)
+  const [period, setPeriod] = useState(DAILY)
+  const { t, i18n } = useTranslation()
 
   const getProductionData = () => {
     ovapi.productionData().then((data) => {
@@ -27,6 +42,40 @@ const ChartProductionData = () => {
     getProductionData()
   }, [])
 
-  return <Chart period={'DAILY'} data={productionData} legend={true} type={'LINE'} />
+  return <>
+
+<div style={{width: '100%', display: 'flex', justifyContent: 'flex-end'}}>
+    <ToggleButtonGroup
+      color="primary"
+      value={period}
+      exclusive
+      onChange={(event, value) => {
+        setPeriod(value)
+      }}
+      aria-label="Platform"
+    >
+      <ToggleButton value={DAILY}>{t('CHART_PERIOD_DIARY')}</ToggleButton>
+      <ToggleButton value={WEEKLY}>{t('CHART_PERIOD_WEEKLY')}</ToggleButton>
+      <ToggleButton value={MONTHLY}>{t('CHART_PERIOD_MONTHLY')}</ToggleButton>
+      <ToggleButton value={YEARLY}>{t('CHART_PERIOD_YEARLY')}</ToggleButton>
+    </ToggleButtonGroup>
+
+    <Grid component="label" container alignItems="center" spacing={1}>
+      <Grid item>Line</Grid>
+      <Grid item>
+        <Switch
+          checked={type}
+          value={BAR}
+          onChange={(event,value) => {
+            setType(value)
+          }}
+        />
+      </Grid>
+      <Grid item>Bar</Grid>
+    </Grid>
+    </div>
+    <Chart period={period} data={productionData} legend={true} type={type} lang={i18n.language} />
+  </>
+
 }
 export default ChartProductionData
