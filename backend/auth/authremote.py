0f02ea932cffd4f58be912ac1d9ef7b269de4b76
marta197 <marta.fernandez@somenergia.coop>
2024-03-15 09:34:30 +0100

    🎩 spike authentik
    Co-authored-by: Joana Figueira <joana.figueira@somenergia.coop>

--- backend/auth/authremote.py
+++ backend/auth/authremote.py
@@ -8,15 +8,19 @@ from fastapi_oauth2.client import OAuth2Client
 from fastapi_oauth2.config import OAuth2Config
 from fastapi_oauth2.security import OAuth2
 from fastapi.security.utils import get_authorization_scheme_param
-from social_core.backends.google import GoogleOAuth2
 from pydantic import EmailStr
+from social_core.backends.google import GoogleOAuth2
+from social_core.backends.oauth import BaseOAuth2
 from jose import JWTError, jwt
 from consolemsg import error
 from ..datasources import user_info
 from .authentik.user_provision import UserProvision
 from .common import auth_error, provisioning_apikey
 
-JWT_ALGORITHM = 'HS256'
+# import os
+# os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+JWT_ALGORITHM = "HS256"
 
 oauth2 = OAuth2()
 
@@ -29,9 +33,13 @@ def forbidden_error(message):
     )
 
 async def validated_user(authorization: str = Depends(oauth2)):
+    import pdb
+
+    pdb.set_trace()
     schema, token = get_authorization_scheme_param(authorization)
     if not authorization or schema.lower() != "bearer":
-        if not oauth2.auto_error: return None
+        if not oauth2.auto_error:
+            return None
         raise auth_error("Not authenticated")
     try:
         payload = jwt.decode(
@@ -43,15 +51,16 @@ async def validated_user(authorization: str = Depends(oauth2)):
         raise auth_error(f"Token decoding failed: {e}")
     return payload
 
+
 async def validated_staff(user: dict = Depends(validated_user)):
-    if 'staff' not in user.get('roles', []):
+    if "staff" not in user.get("roles", []):
         raise forbidden_error(r"{user['username']} is not staff")
     return user
 
 
 def on_auth(auth, user):
     print("on_auth", auth, user)
-    username = user.get('username', user.get('email'))
+    username = user.get("username", user.get("email"))
     if not username:
         auth_error(f"Expected token keys not found in: {user}")
     info = user_info(username)
@@ -60,12 +69,38 @@ def on_auth(auth, user):
     print("on auth returns", info)
     return info
 
+
+# class AuthentikOauth2(KeycloakOAuth2):
+class AuthentikOauth2(BaseOAuth2):
+    name = "authentik"
+    AUTHORIZATION_URL = "http://localhost:80/application/o/authorize/"
+    ACCESS_TOKEN_URL = "http://localhost:80/application/o/token/"
+    ACCESS_TOKEN_METHOD = "POST"
+    REVOKE_TOKEN_URL = "http://localhost:80/application/o/revoke"
+    REVOKE_TOKEN_METHOD = "GET"
+    DEFAULT_SCOPE = ["test"]
+    REDIRECT_STATE = False
+    # EXTRA_DATA = [("expires_in", "expires"), ("refresh_token", "refresh_token")]
+
+
+def OurClaims(Claims):
+
+    def __init__(self, seq=None, **kwargs) -> None:
+        super().__init__(seq or {}, **kwargs)
+        self["display_name"] = kwargs.get("display_name", self.get("name", "nickname"))
+        self["identity"] = kwargs.get(
+            "identity", self.get("preferred_username", "username")
+        )
+        self["picture"] = kwargs.get("picture", self.get("picture", "picture"))
+        self["email"] = kwargs.get("email", self.get("email", "email"))
+
+
 def setup_auth(app):
     config = OAuth2Config(
         # Forbids http (not https) queries to access the cookie
         # TODO: Only false for development
-        allow_http=False,
-        #enable_ssr=False,
+        allow_http=True,
+        # enable_ssr=False,
         jwt_secret=os.getenv("JWT_SECRET"),
         jwt_expires=os.getenv("JWT_EXPIRES", 300),
         jwt_algorithm=JWT_ALGORITHM, # Force
@@ -76,13 +111,29 @@ def setup_auth(app):
                 client_secret=os.getenv("OAUTH2_GOOGLE_CLIENT_SECRET"),
                 scope=["openid", "profile", "email"],
                 # TODO: This is required to work with proxy and not always work
-                #redirect_uri='/',
-                redirect_uri='http://localhost:5173/',
+                # redirect_uri='/',
+                redirect_uri="http://localhost:5173/",
                 claims=Claims(
                     identity=lambda user: f"{user.provider}:{user.sub}",
                 ),
             ),
-        ]
+            OAuth2Client(
+                backend=AuthentikOauth2,
+                client_id=os.getenv("OAUTH2_AUTHENTIK_CLIENT_ID"),
+                client_secret=os.getenv("OAUTH2_AUTHENTIK_CLIENT_SECRET"),
+                # scope=["test", "openid", "profile", "email"],
+                scope=["test"],
+                # TODO: This is required to work with proxy and not always work
+                # redirect_uri='/',
+                redirect_uri="http://localhost:5173/",
+                claims=Claims(
+                    identity=lambda user: f"{user.provider}:{user.sub}",
+                    email=lambda user: f"{user.email}",
+                    display_name=lambda user: f"{user.display_name}",
+                    # identity=lambda user: f"{user.email}",
+                ),
+            ),
+        ],
     )
     @app.post('/api/auth/somenergia/provisioning')
     def somenergia_auth_provision_user(
---
+++ frontend/src/components/AuthProviderOidc.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import useLocalStorage from '../hooks/LocalStorage'
+import { useAuth as useAuthOidc } from 'react-oidc-context'
+import ov from '../services/ovapi'
+// import { useCookies } from 'react-cookie'
+
+const noFunction = () => undefined
+
+const AuthContext = React.createContext({
+  login: noFunction,
+  logout: noFunction,
+  reloadUser: noFunction,
+  currentUser: null,
+})
+
+function AuthProvider({ children }) {
+  const [user, setUser] = useLocalStorage('user', null)
+  const auth = useAuthOidc()
+  const [token, setToken] = useState(null)
+  console.log('AUTH en AuthProvider:', auth)
+
+  let currentUser = null
+  try {
+    currentUser = JSON.parse(user)
+  } catch (e) {
+    console.error('Parsing user info', e)
+  }
+  const setCurrentUser = (user) => setUser(JSON.stringify(user))
+
+  const login = React.useCallback(() => {
+    console.log('login!')
+    auth.signinPopup()
+    reloadUser()
+  }, [])
+
+  const logout = React.useCallback(() => {
+    console.log('logout')
+    setCurrentUser(null)
+    auth.signoutPopup()
+  }, [])
+
+  function error(message) {
+    console.error('Auth Error:', message)
+    return null
+  }
+
+  const reloadUser = React.useCallback(() => {
+    console.log('reload user')
+    ov.currentUser().then((user) => setCurrentUser(user))
+  })
+
+  React.useEffect(() => {
+    reloadUser()
+  }, [])
+
+  React.useEffect(() => {
+    if (!token && auth?.user?.access_token) {
+      setToken(auth.user.access_token)
+      document.cookie = `Authorization=Bearer ${auth.user.access_token}`
+    }
+    token && setToken(null)
+  }, [auth])
+
+  return (
+    <AuthContext.Provider
+      value={{
+        login,
+        logout,
+        currentUser,
+        reloadUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+const useAuth = () => React.useContext(AuthContext)
+
+export { AuthProvider, useAuth }
+export default AuthProvider
