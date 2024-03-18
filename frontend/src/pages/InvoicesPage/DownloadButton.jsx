fc338c71e59649987f5478e14abbcc8444a0dafb
David García Garzón <david.garcia@somenergia.coop>
2024-03-18 17:17:33 +0100

    ♻️ Reusing DownloadButton for DownloadZipButton

--- frontend/src/pages/InvoicesPage/DownloadButton.jsx
+++ frontend/src/pages/InvoicesPage/DownloadButton.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import IconButton from '@mui/material/IconButton'
 import Tooltip from '@mui/material/Tooltip'
 import CircularProgress from '@mui/material/CircularProgress'
-import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf'
+import DownloadIcon from '@mui/icons-material/Download'
 import CheckIcon from '@mui/icons-material/Check'
 import ErrorIcon from '@mui/icons-material/Error'
 import { useTranslation } from 'react-i18next'
+import messages from '../../services/messages'
 import ovapi from '../../services/ovapi'
 
 // States
@@ -13,26 +14,27 @@ const idle = 'idle'
 const done = 'done'
 const inprogress = 'inprogress'
 
-export default function DownloadButton({ context, title, size='small' }) {
+export default function DownloadButton({ context, title, action, size='small', Icon=DownloadIcon }) {
   const { t } = useTranslation()
   const [state, setState] = React.useState(idle)
   const [error, setError] = React.useState(undefined)
 
-  async function onDownloadPdf() {
+  async function download() {
     setState(inprogress)
     setError(undefined)
     try {
-      await ovapi.invoicePdf(context.invoice_number)
+      await action(context)
       setState(done)
     } catch (error) {
       setState(done)
       setError(error)
+      messages.error(error, {context: "Downloading"})
     }
   }
   async function handleClick(ev) {
     ev.stopPropagation()
     if (state === inprogress) return
-    if (state === idle) return await onDownloadPdf()
+    if (state === idle) return await download()
     setState(idle)
     setError(undefined)
   }
@@ -52,13 +54,13 @@ export default function DownloadButton({ context, title, size='small' }) {
     <Tooltip title={tooltip} sx={{ backgroundColor: color }}>
       <IconButton size={size} onClick={handleClick} {...{ color }}>
         {state === idle ? (
-          <PictureAsPdfIcon fontSize="inherit"/>
+          <Icon fontSize="inherit"/>
         ) : state == inprogress ? (
           <CircularProgress size={size==='small'?18:24} />
         ) : error ? (
-          <ErrorIcon  fontSize="inherit"/>
+          <ErrorIcon fontSize="inherit"/>
         ) : (
-          <CheckIcon fontSize="inherit" />
+          <CheckIcon fontSize="inherit"/>
         )}
       </IconButton>
     </Tooltip>
