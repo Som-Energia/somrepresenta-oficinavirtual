7bd0cb02c9672e8c15c554701c3ccfa3f6381ea6
David García Garzón <david.garcia@somenergia.coop>
2024-05-15 12:27:36 +0200

    ♻️ Moved ChartProductionData to page folder
    Co-authored-by: javikalsan <jcalzadosanchez@gmail.com>

--- frontend/src/pages/ProductionPage/index.jsx
+++ frontend/src/pages/ProductionPage/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import Container from '@mui/material/Container'
-import ChartProductionData from '../../components/ChartProductionData'
+import ChartProductionData from './ChartProductionData'
 
 export default function ProductionPage(params) {
   const { t } = useTranslation()
