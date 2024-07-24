ffe5ab92867fb805642cb43ae3d18fc0e5e5a5dc
Marite Guerrieri <marite.guerrieri@somenergia.coop>
2024-07-24 11:20:22 +0200

    ✨ Authentik user retrieve, create, remove
    Co-authored-by: David García Garzón <david.garcia@somenergia.coop>

--- backend/auth/authentik/user_provision.py
+++ backend/auth/authentik/user_provision.py
@@ -1,5 +1,26 @@
-import requests
+import httpx
+from yamlns import ns
 import os
+from pydantic import (
+    BaseModel,
+    AwareDatetime,
+    EmailStr,
+    UUID4,
+)
+
+debug = True
+
+class NewUser(BaseModel):
+    username: str
+    name: str
+    is_active: bool
+    last_login: AwareDatetime
+    groups: list[UUID4]
+    email: EmailStr
+    attributes: dict
+    path: str
+    type: str
+
 
 class UserProvision:
 
@@ -8,18 +29,44 @@ class UserProvision:
         TOKEN=os.environ.get("AUTHENTIK_TOKEN")
         full_url = f"{BASE_URL}/api/v3/{url}"
         headers = {
+            'Content-Type': 'application/json',
             'Accept': 'application/json',
             'Authorization': f"Bearer {TOKEN}"
         }
         method = method if method is not None else "POST" if payload else "GET"
+        #debug and print(f"{method} {full_url}\n{payload and ns(payload).dump()}")
 
-        response = requests.request(method, full_url, headers=headers, data=payload)
-        return response.json()
+        response = httpx.request(method, full_url, headers=headers, data=payload)
+
+        response.raise_for_status()
+
+        print('RESPONSE:',response.text)
+        return response.json() if response.text else None
 
 
     def version(self):
         return self._api("admin/version/")
 
 
+    def retrieve(self, user_id):
+        try:
+            return self._api(f"core/users/{user_id}/", payload={}, method="GET")
+        except httpx.HTTPError as e:
+            if e.response.status_code == 404:
+                return None
+            raise
 
+    def create(self, user: NewUser):
+        try:
+            return self._api(f"core/users/", payload=user.json(), method="POST")
+        except httpx.HTTPError as e:
+            print(e.response.status_code, e.response.text)
+            raise
 
+    def remove(self, user_id):
+        try:
+            return self._api(f"core/users/{user_id}/", payload={}, method="DELETE")
+        except httpx.HTTPError as e:
+            if e.response.status_code == 404:
+                return None
+            raise
--- backend/auth/authentik/user_provision_test.py
+++ backend/auth/authentik/user_provision_test.py
@@ -1,19 +1,49 @@
 import unittest
-from .user_provision import UserProvision
+from .user_provision import UserProvision, NewUser
 from dotenv import load_dotenv
-
+import os
+import datetime
 
 class UserProvision_Test(unittest.TestCase):
     from yamlns.testutils import assertNsEqual
+
+    non_existing_id = 88888888
+
     def setUp(self):
         # TODO: restore environment after tests
         load_dotenv()
+        self.group = os.environ.get("AUTHENTIK_GROUP_ID")
 
-    def test(self):
+    def test__version__returns_current(self):
         api = UserProvision()
         result = api.version()
         self.assertIn('version_current', result)
         
+    def test__retrieve__non_existing__returns_none(self):
+        api = UserProvision()
+        result = api.retrieve(self.non_existing_id)
+        self.assertIsNone(result)
+        
+    def test__create_and_delete(self):
+        api = UserProvision()
+
+        new_user = api.create(NewUser(
+            username="non-existing-user",
+            name="Non Existing User",
+            is_active=True,
+            last_login=datetime.datetime.now(datetime.timezone.utc) ,
+            groups=[self.group],
+            email="a@a.net",
+            attributes={},
+            path="algo",
+            type="internal",
+        ))
+        id = new_user['pk']
+        result = api.retrieve(id)
+        self.assertEqual(result.get('name'), "Non Existing User")
+        api.remove(id)
+        result = api.retrieve(id)
+        self.assertIsNone(result)
 
 
 
