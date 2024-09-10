cd92c35d8a63f44409b72b1accc4427bfcefdb38
David García Garzón <david.garcia@somenergia.coop>
2024-09-10 09:17:08 +0200

    ♻️ Specific Theme

---
+++ frontend/src/components/SomRepresentaTheme.jsx
@@ -0,0 +1,6 @@
+import SomEnergiaTheme from '@somenergia/somenergia-ui/SomEnergiaTheme'
+
+// Modify coorporative atoms with specific atoms for the app
+export default SomRepresentaTheme = {
+  ...SomEnergiaTheme
+}
